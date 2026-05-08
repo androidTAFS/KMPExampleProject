b44280d492d1f0876257aaf71e0670acafa71f0d
arteml <lapukhinaa101@gmail.com>
2026-05-08 10:13:24 +0300

    lessons update

---
+++ composeApp/src/commonMain/kotlin/io/androidtafs/exampleproject/ui/feature/practice_holder/task_4/Emojis.kt
@@ -0,0 +1,13 @@
+package io.androidtafs.exampleproject.ui.feature.practice_holder.task_4
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+// 🎵
+// ⏮
+// ▶
+// ⏭
+// ⚔
